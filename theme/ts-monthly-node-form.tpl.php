6c2dfe5d13fb78dc90fda4fb39c98e4544688719
Scott Rouse <scottwrouse@gmail.com>
2012-10-04 11:02:55 -0600

    Created placeholder tpl files for other node add/edit

---
+++ theme/ts-monthly-node-form.tpl.php
@@ -0,0 +1,8 @@
+<!-- @todo - remove before publishing -->
+<div style="border:2px dashed red; padding: 10px; margin: 10px; background-color: #ccc;">
+
+<p>This is the "ts-monthly-node-form.tpl.php" template file output.</p>
+
+</div>
+
+<?php print drupal_render_children($form); ?>
