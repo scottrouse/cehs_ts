1590e31da204d1e483285d15cd5db5525b2dafcb
Brad Wallace <Brad@daybreakstudios.net>
2012-10-11 15:58:50 -0400

    Fields 08-31 added on line 45-188
    Fields 08-31 added on line 45-188

--- js/biweekly.js
+++ js/biweekly.js
@@ -42,6 +42,150 @@ jQuery(document).ready(function($){
     var s = Number($('#edit-field-s07-und-0-value').val());
     $('#edit-field-t07-und-0-value').val(r+a+s);
   });
+  $('#edit-field-r08-und-0-value,#edit-field-a08-und-0-value,#edit-field-s08-und-0-value,#edit-field-t08-und-0-value').change(function() {
+    var r = Number($('#edit-field-r08-und-0-value').val());
+    var a = Number($('#edit-field-a08-und-0-value').val());
+    var s = Number($('#edit-field-s08-und-0-value').val());
+    $('#edit-field-t08-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r09-und-0-value,#edit-field-a09-und-0-value,#edit-field-s09-und-0-value,#edit-field-t09-und-0-value').change(function() {
+    var r = Number($('#edit-field-r09-und-0-value').val());
+    var a = Number($('#edit-field-a09-und-0-value').val());
+    var s = Number($('#edit-field-s09-und-0-value').val());
+    $('#edit-field-t09-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r10-und-0-value,#edit-field-a10-und-0-value,#edit-field-s10-und-0-value,#edit-field-t10-und-0-value').change(function() {
+    var r = Number($('#edit-field-r10-und-0-value').val());
+    var a = Number($('#edit-field-a10-und-0-value').val());
+    var s = Number($('#edit-field-s10-und-0-value').val());
+    $('#edit-field-t10-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r11-und-0-value,#edit-field-a11-und-0-value,#edit-field-s11-und-0-value,#edit-field-t11-und-0-value').change(function() {
+    var r = Number($('#edit-field-r11-und-0-value').val());
+    var a = Number($('#edit-field-a11-und-0-value').val());
+    var s = Number($('#edit-field-s11-und-0-value').val());
+    $('#edit-field-t11-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r12-und-0-value,#edit-field-a12-und-0-value,#edit-field-s12-und-0-value,#edit-field-t12-und-0-value').change(function() {
+    var r = Number($('#edit-field-r12-und-0-value').val());
+    var a = Number($('#edit-field-a12-und-0-value').val());
+    var s = Number($('#edit-field-s12-und-0-value').val());
+    $('#edit-field-t12-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r13-und-0-value,#edit-field-a13-und-0-value,#edit-field-s13-und-0-value,#edit-field-t13-und-0-value').change(function() {
+    var r = Number($('#edit-field-r13-und-0-value').val());
+    var a = Number($('#edit-field-a13-und-0-value').val());
+    var s = Number($('#edit-field-s13-und-0-value').val());
+    $('#edit-field-t13-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r14-und-0-value,#edit-field-a14-und-0-value,#edit-field-s14-und-0-value,#edit-field-t14-und-0-value').change(function() {
+    var r = Number($('#edit-field-r14-und-0-value').val());
+    var a = Number($('#edit-field-a14-und-0-value').val());
+    var s = Number($('#edit-field-s14-und-0-value').val());
+    $('#edit-field-t14-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r15-und-0-value,#edit-field-a15-und-0-value,#edit-field-s15-und-0-value,#edit-field-t15-und-0-value').change(function() {
+    var r = Number($('#edit-field-r15-und-0-value').val());
+    var a = Number($('#edit-field-a15-und-0-value').val());
+    var s = Number($('#edit-field-s15-und-0-value').val());
+    $('#edit-field-t15-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r16-und-0-value,#edit-field-a16-und-0-value,#edit-field-s16-und-0-value,#edit-field-t16-und-0-value').change(function() {
+    var r = Number($('#edit-field-r16-und-0-value').val());
+    var a = Number($('#edit-field-a16-und-0-value').val());
+    var s = Number($('#edit-field-s16-und-0-value').val());
+    $('#edit-field-t16-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r17-und-0-value,#edit-field-a17-und-0-value,#edit-field-s17-und-0-value,#edit-field-t17-und-0-value').change(function() {
+    var r = Number($('#edit-field-r17-und-0-value').val());
+    var a = Number($('#edit-field-a17-und-0-value').val());
+    var s = Number($('#edit-field-s17-und-0-value').val());
+    $('#edit-field-t17-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r18-und-0-value,#edit-field-a18-und-0-value,#edit-field-s18-und-0-value,#edit-field-t18-und-0-value').change(function() {
+    var r = Number($('#edit-field-r18-und-0-value').val());
+    var a = Number($('#edit-field-a18-und-0-value').val());
+    var s = Number($('#edit-field-s18-und-0-value').val());
+    $('#edit-field-t18-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r19-und-0-value,#edit-field-a19-und-0-value,#edit-field-s19-und-0-value,#edit-field-t19-und-0-value').change(function() {
+    var r = Number($('#edit-field-r19-und-0-value').val());
+    var a = Number($('#edit-field-a19-und-0-value').val());
+    var s = Number($('#edit-field-s19-und-0-value').val());
+    $('#edit-field-t19-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r20-und-0-value,#edit-field-a20-und-0-value,#edit-field-s20-und-0-value,#edit-field-t20-und-0-value').change(function() {
+    var r = Number($('#edit-field-r20-und-0-value').val());
+    var a = Number($('#edit-field-a20-und-0-value').val());
+    var s = Number($('#edit-field-s20-und-0-value').val());
+    $('#edit-field-t20-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r21-und-0-value,#edit-field-a21-und-0-value,#edit-field-s21-und-0-value,#edit-field-t21-und-0-value').change(function() {
+    var r = Number($('#edit-field-r21-und-0-value').val());
+    var a = Number($('#edit-field-a21-und-0-value').val());
+    var s = Number($('#edit-field-s21-und-0-value').val());
+    $('#edit-field-t21-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r22-und-0-value,#edit-field-a22-und-0-value,#edit-field-s22-und-0-value,#edit-field-t22-und-0-value').change(function() {
+    var r = Number($('#edit-field-r22-und-0-value').val());
+    var a = Number($('#edit-field-a22-und-0-value').val());
+    var s = Number($('#edit-field-s22-und-0-value').val());
+    $('#edit-field-t22-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r23-und-0-value,#edit-field-a23-und-0-value,#edit-field-s23-und-0-value,#edit-field-t23-und-0-value').change(function() {
+    var r = Number($('#edit-field-r23-und-0-value').val());
+    var a = Number($('#edit-field-a23-und-0-value').val());
+    var s = Number($('#edit-field-s23-und-0-value').val());
+    $('#edit-field-t23-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r24-und-0-value,#edit-field-a24-und-0-value,#edit-field-s24-und-0-value,#edit-field-t24-und-0-value').change(function() {
+    var r = Number($('#edit-field-r24-und-0-value').val());
+    var a = Number($('#edit-field-a24-und-0-value').val());
+    var s = Number($('#edit-field-s24-und-0-value').val());
+    $('#edit-field-t24-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r25-und-0-value,#edit-field-a25-und-0-value,#edit-field-s25-und-0-value,#edit-field-t25-und-0-value').change(function() {
+    var r = Number($('#edit-field-r25-und-0-value').val());
+    var a = Number($('#edit-field-a25-und-0-value').val());
+    var s = Number($('#edit-field-s25-und-0-value').val());
+    $('#edit-field-t25-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r26-und-0-value,#edit-field-a26-und-0-value,#edit-field-s26-und-0-value,#edit-field-t26-und-0-value').change(function() {
+    var r = Number($('#edit-field-r26-und-0-value').val());
+    var a = Number($('#edit-field-a26-und-0-value').val());
+    var s = Number($('#edit-field-s26-und-0-value').val());
+    $('#edit-field-t26-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r27-und-0-value,#edit-field-a27-und-0-value,#edit-field-s27-und-0-value,#edit-field-t27-und-0-value').change(function() {
+    var r = Number($('#edit-field-r27-und-0-value').val());
+    var a = Number($('#edit-field-a27-und-0-value').val());
+    var s = Number($('#edit-field-s27-und-0-value').val());
+    $('#edit-field-t27-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r28-und-0-value,#edit-field-a28-und-0-value,#edit-field-s28-und-0-value,#edit-field-t28-und-0-value').change(function() {
+    var r = Number($('#edit-field-r28-und-0-value').val());
+    var a = Number($('#edit-field-a28-und-0-value').val());
+    var s = Number($('#edit-field-s28-und-0-value').val());
+    $('#edit-field-t28-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r29-und-0-value,#edit-field-a29-und-0-value,#edit-field-s29-und-0-value,#edit-field-t29-und-0-value').change(function() {
+    var r = Number($('#edit-field-r29-und-0-value').val());
+    var a = Number($('#edit-field-a29-und-0-value').val());
+    var s = Number($('#edit-field-s29-und-0-value').val());
+    $('#edit-field-t29-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r30-und-0-value,#edit-field-a30-und-0-value,#edit-field-s30-und-0-value,#edit-field-t30-und-0-value').change(function() {
+    var r = Number($('#edit-field-r30-und-0-value').val());
+    var a = Number($('#edit-field-a30-und-0-value').val());
+    var s = Number($('#edit-field-s30-und-0-value').val());
+    $('#edit-field-t30-und-0-value').val(r+a+s);
+  });
+  $('#edit-field-r31-und-0-value,#edit-field-a31-und-0-value,#edit-field-s31-und-0-value,#edit-field-t31-und-0-value').change(function() {
+    var r = Number($('#edit-field-r31-und-0-value').val());
+    var a = Number($('#edit-field-a31-und-0-value').val());
+    var s = Number($('#edit-field-s31-und-0-value').val());
+    $('#edit-field-t31-und-0-value').val(r+a+s);
+  });
   /* @todo - BRAD - Copy the 6 lines above (from $('#edit-field... to the }); right above this comment). Paste and change the
   relevant parts through field 31. */
   
