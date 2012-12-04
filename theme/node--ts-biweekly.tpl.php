f44ca0aae1fff8c095654ef32229ff5765e1c699
Brad Wallace <Brad@daybreakstudios.net>
2012-12-04 12:24:49 -0500

    edited theme grant on biweekly timesheet
    edited theme grant on biweekly timesheet to change field_grant01 to
    field_grant_01 for all fields.

--- theme/node--ts-biweekly.tpl.php
+++ theme/node--ts-biweekly.tpl.php
@@ -360,53 +360,53 @@
     </thead>
     <tbody>
       <tr>
-        <td><?php print render($content['field_grant01_ref']); ?></td>
-        <td><?php print render($content['field_grant01_perc']); ?></td>
+        <td><?php print render($content['field_grant_01_ref']); ?></td>
+        <td><?php print render($content['field_grant_01_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant02_ref']); ?></td>
-        <td><?php print render($content['field_grant02_perc']); ?></td>
+        <td><?php print render($content['field_grant_02_ref']); ?></td>
+        <td><?php print render($content['field_grant_02_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant03_ref']); ?></td>
-        <td><?php print render($content['field_grant03_perc']); ?></td>
+        <td><?php print render($content['field_grant_03_ref']); ?></td>
+        <td><?php print render($content['field_grant_03_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant04_ref']); ?></td>
-        <td><?php print render($content['field_grant04_perc']); ?></td>
+        <td><?php print render($content['field_grant_04_ref']); ?></td>
+        <td><?php print render($content['field_grant_04_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant05_ref']); ?></td>
-        <td><?php print render($content['field_grant05_perc']); ?></td>
+        <td><?php print render($content['field_grant_05_ref']); ?></td>
+        <td><?php print render($content['field_grant_05_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant06_ref']); ?></td>
-        <td><?php print render($content['field_grant06_perc']); ?></td>
+        <td><?php print render($content['field_grant_06_ref']); ?></td>
+        <td><?php print render($content['field_grant_06_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant07_ref']); ?></td>
-        <td><?php print render($content['field_grant07_perc']); ?></td>
+        <td><?php print render($content['field_grant_07_ref']); ?></td>
+        <td><?php print render($content['field_grant_07_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant08_ref']); ?></td>
-        <td><?php print render($content['field_grant08_perc']); ?></td>
+        <td><?php print render($content['field_grant_08_ref']); ?></td>
+        <td><?php print render($content['field_grant_08_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant09_ref']); ?></td>
-        <td><?php print render($content['field_grant09_perc']); ?></td>
+        <td><?php print render($content['field_grant_09_ref']); ?></td>
+        <td><?php print render($content['field_grant_09_perc']); ?></td>
       </tr>
       <tr>
       <tr>
-        <td><?php print render($content['field_grant10_ref']); ?></td>
-        <td><?php print render($content['field_grant10_perc']); ?></td>
+        <td><?php print render($content['field_grant_10_ref']); ?></td>
+        <td><?php print render($content['field_grant_10_perc']); ?></td>
       </tr>
       <tr>
         <th>Total</th>
